d6542b51a4ad9c8ee1ea56ce9b79cc16d59a3be0
Louis Birla <louis@birla.io>
2020-09-07 13:33:43 -0500

    undefined -> null

--- api/app.ts
+++ api/app.ts
@@ -27,7 +27,7 @@ settings.change({
       introspection: true,
     },
     cors: {
-      origin: (origin, callback) => callback(undefined, callback),
+      origin: (origin, callback) => callback(null, callback),
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       preflightContinue: false,
       optionsSuccessStatus: 204,
