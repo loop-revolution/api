95ff070d35f47c8ea4a55f46595806181035a987
Louis Birla <louis@birla.io>
2020-09-07 16:44:10 +0000

    API-2 #comment Set up some interactivity for app connections

---
+++ api/graphql/count.ts
@@ -0,0 +1,36 @@
+// This is for setting up connections with GraphQL
+import { schema } from "nexus";
+
+const data = {
+  count: 1
+}
+
+/** Returns a list of blocks */
+schema.extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('count', {
+      type: 'Int',
+      resolve(_root, _args, ctx) {
+        return data.count
+      }
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('updateCount', {
+      type: 'Int',
+      args: {
+        by: schema.intArg({required: true})
+      },
+      resolve(_root, { by }, ctx) {
+        data.count += by
+        return data.count
+      }
+    })
+  },
+})
+
