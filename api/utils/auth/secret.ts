b6ffe268733d40771f555bd2045a2819b0598ba9
Louis Birla <louis.birla@gmail.com>
2021-01-05 15:51:41 -0600

    Removes all nexus & TS files

--- api/utils/auth/secret.ts
+++
@@ -1,7 +0,0 @@
-/** For signing JWTs */
-export const appSecret = process.env.APP_SECRET as string
-
-const minSecretLength = 30
-if (!appSecret || appSecret.length < minSecretLength) {
-  throw new Error('You must provide an APP_SECRET environment variable to set up JWT.')
-}
--- api/utils/errors/password.ts
+++
@@ -1,7 +0,0 @@
-/** Simple invalid password */
-export class InvalidPasswordError extends Error {
-  constructor(message?: string) {
-    super(message)
-    this.name = 'InvalidPasswordError'
-  }
-}
