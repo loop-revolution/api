aed247841011f896e1443d0027c88b51c92f7939
Louis Birla <louis.birla@gmail.com>
2021-02-25 14:26:52 -0600

    refactor: add helper functions

--- block-tools/src/display_api/component/button.rs
+++ block-tools/src/display_api/component/button.rs
@@ -5,12 +5,12 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct ButtonComponent {
-	pub interact: Option<ActionObject>,
-	pub text: String,
 	pub color_scheme: Option<String>,
-	pub variant: Option<ButtonVariant>,
 	pub icon: Option<Icon>,
+	pub interact: Option<ActionObject>,
 	pub size: Option<String>,
+	pub text: String,
+	pub variant: Option<ButtonVariant>,
 }
 
 impl DisplayComponent for ButtonComponent {
@@ -20,6 +20,26 @@ impl DisplayComponent for ButtonComponent {
 
 	fn args(&self) -> &dyn Serializable {
 		self
+	}
+}
+
+impl ButtonComponent {
+	pub fn new(text: &str) -> Self {
+		ButtonComponent {
+			color_scheme: None,
+			icon: None,
+			interact: None,
+			size: None,
+			text: text.into(),
+			variant: None,
+		}
+	}
+
+	pub fn interact(self, action: ActionObject) -> Self {
+		ButtonComponent {
+			interact: Some(action),
+			..self
+		}
 	}
 }
 
