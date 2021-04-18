0ec8885b2a1cd9e9dff1e779ad7eaf282af6e9db
Louis Birla <louis.birla@gmail.com>
2021-04-18 00:00:21 -0500

    feat: setup habit block structure

--- block-types/src/delegation/display.rs
+++ block-types/src/delegation/display.rs
@@ -18,6 +18,7 @@ pub fn delegate_page_display(block: &Block, context: &Context) -> Result<Display
 		BlockTypes::Text => text_block::TextBlock::page_display(block, context),
 		BlockTypes::Group => group_block::GroupBlock::page_display(block, context),
 		BlockTypes::Document => document_block::DocumentBlock::page_display(block, context),
+		BlockTypes::Habit => habit_block::HabitBlock::page_display(block, context),
 		BlockTypes::Invalid(name) => Ok(DisplayObject::new(
 			TextComponent {
 				color: Some("#ff0000".to_string()),
@@ -35,6 +36,7 @@ pub fn delegate_embed_display(block: &Block, context: &Context) -> DisplayCompon
 		BlockTypes::Text => text_block::TextBlock::embed_display(block, context),
 		BlockTypes::Group => group_block::GroupBlock::embed_display(block, context),
 		BlockTypes::Document => document_block::DocumentBlock::embed_display(block, context),
+		BlockTypes::Habit => habit_block::HabitBlock::embed_display(block, context),
 		BlockTypes::Invalid(name) => TextComponent {
 			color: Some("#ff0000".to_string()),
 			..TextComponent::new(format!("Invalid block type '{}'", name))
@@ -54,6 +56,7 @@ pub fn delegate_creation_display(
 		BlockTypes::Text => text_block::TextBlock::create_display(context, user_id),
 		BlockTypes::Group => group_block::GroupBlock::create_display(context, user_id),
 		BlockTypes::Document => document_block::DocumentBlock::create_display(context, user_id),
+		BlockTypes::Habit => habit_block::HabitBlock::create_display(context, user_id),
 		BlockTypes::Invalid(name) => Err(BlockError::TypeExist(name).into()),
 	}
 }
@@ -69,6 +72,7 @@ pub fn delegate_block_name(
 		BlockTypes::Text => text_block::TextBlock::block_name(block, context),
 		BlockTypes::Group => group_block::GroupBlock::block_name(block, context),
 		BlockTypes::Document => document_block::DocumentBlock::block_name(block, context),
+		BlockTypes::Habit => habit_block::HabitBlock::block_name(block, context),
 		BlockTypes::Invalid(name) => Err(BlockError::TypeExist(name).into()),
 	}
 }
@@ -80,6 +84,7 @@ pub fn delegate_block_icon(block_type: impl ToString) -> Option<Icon> {
 		BlockTypes::Text => text_block::TextBlock::info().icon,
 		BlockTypes::Group => group_block::GroupBlock::info().icon,
 		BlockTypes::Document => document_block::DocumentBlock::info().icon,
+		BlockTypes::Habit => habit_block::HabitBlock::info().icon,
 		BlockTypes::Invalid(_) => return None,
 	})
 }
